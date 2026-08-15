321d33b9d998fdee1cab3bf471734f2830aa6103
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:42:30 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,16 +11,3 @@ pacman -Syu --noconfirm xorg-xeyes
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 #get-debloated-pkgs --add-common --prefer-nano
-
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
