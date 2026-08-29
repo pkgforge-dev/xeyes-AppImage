eab58e5c9fe36273ee8f66b0fc1a2d7ef2bb8d07
Link4Electronics <link4electronics@gmail.com>
2026-08-29 15:50:07 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,7 @@ VERSION=$(pacman -Q xorg-xeyes | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ALWAYS_SOFTWARE=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/xeyes
